45cb8f911da05613f10700798b016a8c206acc63
13thlyne-a11y <13thlyne@gmail.com>
2026-05-31 16:15:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,7 @@ async function startCamera() {
 
         const stream =
             await navigator.mediaDevices.getUserMedia({
-                video: {
-                    width: { ideal: 1920 },
-                    height: { ideal: 1080 },
-                    facingMode: {
-                        ideal: "environment"
-                    }
-                },
+                video: true,
                 audio: false
             });
 
@@ -28,43 +22,7 @@ async function startCamera() {
             "에러명: " + err.name +
             "\n메시지: " + err.message
         );
-
-        console.error(err);
     }
 }
 
 startCamera();
-
-document
-.getElementById("captureBtn")
-.addEventListener("click", capture);
-
-function capture() {
-
-    const canvas =
-        document.getElementById("captureCanvas");
-
-    const ctx =
-        canvas.getContext("2d");
-
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-
-    ctx.drawImage(
-        video,
-        0,
-        0,
-        canvas.width,
-        canvas.height
-    );
-
-    const link =
-        document.createElement("a");
-
-    link.download = "ar-photo.png";
-
-    link.href =
-        canvas.toDataURL("image/png");
-
-    link.click();
-}
